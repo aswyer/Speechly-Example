6f03792a691fb4701c1a47dab61dc74027a36835
Andrew Sawyer <ajsawyer3@gmail.com>
2022-06-17 12:26:37 -0400

    Initial Commit

---
+++ Speechly Example/Speechly_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speechly_ExampleApp.swift
+//  Speechly Example
+//
+//  Created by Andrew Sawyer on 6/17/22.
+//
+
+import SwiftUI
+
+@main
+struct Speechly_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
